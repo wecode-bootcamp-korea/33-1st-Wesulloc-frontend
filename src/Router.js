6c2ff92099742809b6f8dc155343142937a378d3
Soojung An <presentnow21@gmail.com>
2022-05-25 07:47:57 +0900

    ADD: 상세페이지 정적 구현

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import ProductDetail from './pages/products/productDetail/ProductDetail';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
