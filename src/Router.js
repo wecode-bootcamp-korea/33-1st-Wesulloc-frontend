67037fec6e7eb38b9ebe310260752f486cde65ce
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-26 01:59:05 +0900

    Modify: Router conflict 수정

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Cart from '../src/pages/cart/Cart';
-
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Cart />} />
+        <Route path="/" />
       </Routes>
     </BrowserRouter>
   );
