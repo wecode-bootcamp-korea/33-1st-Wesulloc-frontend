2ccc8ee82555b8cf5f4cd41fc3138e54c79fe52d
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-26 10:53:42 +0900

    Modify: 모달창 팝업 시 배경 스크롤 방지 기능 추가

--- src/pages/cart/components/ErrorModal.js
+++ src/pages/cart/components/ErrorModal.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import Card from '../../../components/UI/Card';
 
 import './ErrorModal.scss';
 
 const ErrorModal = ({ message, errorHandler }) => {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      `;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
   return (
     <div className="backdrop">
       <Card className="errorModal">
