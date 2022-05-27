25f9662829969cd692f37e634584e05ff04a2c9e
hyeonjoo KIM <hyeonjookim@hyeonjoos-MacBook-Pro.local>
2022-05-27 18:27:59 +0900

    Modify: 라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Cart from './pages/cart/Cart';
-import Terms from './pages/signup/components/Terms';
 
 const Router = () => {
   return (
@@ -10,7 +9,6 @@ const Router = () => {
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/" element={<Terms />} />
       </Routes>
     </BrowserRouter>
   );
