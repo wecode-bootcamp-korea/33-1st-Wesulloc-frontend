5724626cc006133d3fdd7d73e50a67b6527ffc9a
devseop <cocc263@gmail.com>
2022-05-30 16:14:04 +0900

    [33기 이윤섭] ADD: 제품 페이지 슬라이드 버튼 클릭시 슬라이드 이동 기능 추가

--- src/pages/products/components/Slides.js
+++ src/pages/products/components/Slides.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import SlideButton from './SlideButton';
 
 const Slides = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -8,8 +9,20 @@ const Slides = () => {
       setCurrentIndex(currentIndex => {
         return (currentIndex += currentIndex === 3 ? 0 : 1);
       });
-    }, 2000);
+    }, 5000);
   }, [currentIndex]);
+
+  const moveSlide = id => {
+    if (id === 1) {
+      setCurrentIndex(0);
+    } else if (id === 2) {
+      setCurrentIndex(1);
+    } else if (id === 3) {
+      setCurrentIndex(2);
+    } else if (id === 4) {
+      setCurrentIndex(3);
+    }
+  };
 
   return (
     <header className="productHeader">
@@ -17,24 +30,23 @@ const Slides = () => {
         <ul
           className="slidesWrapper"
           style={{
-            transform: `translate3d(${-(55 * currentIndex)}rem, 0, 0)`,
+            transform: `translateX(${-(55 * currentIndex)}rem)`,
           }}
         >
           {SLIDE_LIST.map(data => (
             <li className="slideItem" key={data.id}>
               <div className="slideTextBox">
-                <p className="slideName">{data.name}</p>
-                <p className="slideContent">{data.content}</p>
+                <span className="slideName">{data.name}</span>
+                <span className="slideContent">{data.content1}</span>
+                <span className="slideContent">{data.content2}</span>
               </div>
             </li>
           ))}
         </ul>
       </div>
       <div className="slideButtonsWrapper">
-        {SLIDE_LIST.map((data, index) => (
-          <button className="slideButton" key={data.id}>
-            {data.name}
-          </button>
+        {SLIDE_LIST.map(data => (
+          <SlideButton key={data.id} buttonData={data} moveSlide={moveSlide} />
         ))}
       </div>
     </header>
@@ -45,23 +57,27 @@ const SLIDE_LIST = [
   {
     id: 1,
     name: '명차',
-    content: '고귀하고 품격 있는 티 라이프를 위한 오설록의 명차 라인입니다.',
+    content1: '고귀하고 품격 있는 티 라이프를 위한',
+    content2: '오설록의 명차 라인입니다.',
   },
 
   {
     id: 2,
     name: '녹차/발효차/홍차',
-    content: '유기농 제주 차밭에서 자란 우리 녹차 발효차 라인입니다.',
+    content1: '유기농 제주 차밭에서 자란',
+    content2: '우리 녹차 발효차 라인입니다.',
   },
   {
     id: 3,
     name: '허브티',
-    content: '꽃잎, 과일향이 가득한 풍미가 돋보이는 라인입니다.',
+    content1: '꽃잎, 과일향이 가득한',
+    content2: '풍미가 돋보이는 라인입니다.',
   },
   {
     id: 4,
     name: '블렌디드티',
-    content: '제주의 다양한 품경과 꽃내음을 담았습니다.',
+    content1: '제주의 다양한 품경과',
+    content2: '꽃내음을 담았습니다.',
   },
 ];
 
