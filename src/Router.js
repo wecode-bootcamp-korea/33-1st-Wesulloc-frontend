4854dba38dde51d7a22e6e61dadb56aff26567e6
devseop <cocc263@gmail.com>
2022-06-02 18:58:34 +0900

    [33기 이윤섭] MODIFIED: Router 경로 수정

--- src/Router.js
+++ src/Router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import ProductList from './pages/products/ProductList/ProductList';
-import WeeklyBest from './pages/products/BestProducts/BestProductList';
+import BestProductList from './pages/products/BestProducts/BestProductList';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/ProductList" element={<ProductList />} />
-        <Route path="/Product" element={<WeeklyBest />} />
+        <Route path="/Product" element={<BestProductList />} />
         <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
